054b2fc47912ab53bb6fd8338de60701f732719b
Hank Eichstaedt <hank.eichstaedt@gmail.com>
2026-09-16 15:25:57 -0400

    Update HW2

--- apputil.py
+++ apputil.py
@@ -8,11 +8,6 @@ def ways(n):
     return n // 5 + 1
 
 
-ways(12)
-ways(20)
-ways(3)
-ways(0)
-
 # Exercise two: Scores
 names = np.array(['Hannah', 'Astrid', 'Abdul', 'Mauve', 'Jung'])
 scores = np.array([99, 71, 85, 62, 91])
@@ -23,12 +18,6 @@ def lowest_score(names, scores):
     return names[lowest]
 
 
-lowest_score(names, scores)
-
-
 def sort_names(names, scores):
     order = np.argsort(scores)[::-1]
-    return names[order], scores[order]
-
-
-sort_names(names, scores)
+    return names[order]
