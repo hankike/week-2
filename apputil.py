16504a8401e8b0f54259c4badac0f2c9a16019b0
Hank Eichstaedt <hank.eichstaedt@gmail.com>
2026-09-16 15:07:03 -0400

    Homework 2 Changes

--- apputil.py
+++ apputil.py
@@ -3,11 +3,32 @@ import numpy as np
 
 # update/add code below ...
 
+# Exercise one: Nickels and pennies
 def ways(n):
-    return None
+    return n // 5 + 1
+
+
+ways(12)
+ways(20)
+ways(3)
+ways(0)
+
+# Exercise two: Scores
+names = np.array(['Hannah', 'Astrid', 'Abdul', 'Mauve', 'Jung'])
+scores = np.array([99, 71, 85, 62, 91])
+
 
 def lowest_score(names, scores):
-    return None
+    lowest = np.argmin(scores)
+    return names[lowest]
+
+
+lowest_score(names, scores)
+
 
 def sort_names(names, scores):
-    return None
+    order = np.argsort(scores)[::-1]
+    return names[order], scores[order]
+
+
+sort_names(names, scores)
